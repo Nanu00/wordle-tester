67eb2751e8aec9c2680e2da52a017a47b34bf169
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2022-02-03 22:08:50 +0530

    Updated testing function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::{
     fs,
     process::{
-        Command,
-        Stdio,
-        }, io::{Read, Write},
+        Command, Stdio,
+        },
+    io::{
+        BufRead, BufReader, Write
+    },
+    path::{
+        Path, PathBuf,
+    },
+    sync::mpsc::{
+            Sender, Receiver, channel
+        },
+    thread
 };
 use clap::Parser;
 
@@ -11,53 +20,88 @@ use clap::Parser;
 #[clap(version, about, long_about = None)]
 struct Args {
     /// Path to the programs to run
-    runfile: String,
+    runfile: PathBuf,
 
     /// Path to the words list file
-    wordsfile: String,
+    wordsfile: PathBuf,
 }
 
-fn test(cmd: String, word: String) -> std::io::Result<i32> {
-    let mut child = Command::new(cmd)
+fn start_process(sender: Sender<String>, reciever: Receiver<String>, cmd: &Path, args: Vec<&str>) {
+    let child = Command::new(cmd)
+        .args(args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .spawn()?;
+        .spawn()
+        .expect(&format!("Failed to launch {:?}", cmd));
 
-    let mut child_stdout = child.stdout.take();
-    let mut child_stdin = child.stdin.as_mut().unwrap();
+    println!("Started process: {:?} : {}", cmd, child.id());
+
+    thread::spawn(move || {
+        let mut f = BufReader::new(child.stdout.unwrap());
+        let mut stdin = child.stdin.unwrap();
+        // for line in reciever {
+        //     let mut buf = String::new();
+        //     match f.read_line(&mut buf) {
+        //         Ok(_) => {
+        //             sender.send(buf).unwrap();
+        //         },
+        //         Err(e) => {
+        //             println!("Error!: {:?}", e);
+        //             break;
+        //         }
+        //     }
+        //     stdin.write_all(line.as_bytes()).unwrap();
+        // }
+
+        let mut buf = String::new();
+        while f.read_line(&mut buf).is_ok() {
+            sender.send(buf.clone()).unwrap();
+            let recvd = reciever.recv().unwrap();
+            println!("{}", recvd);
+            stdin.write_all(recvd.as_bytes()).unwrap();
+            buf = String::new();
+        }
+    });
+}
+
+fn test(cmd: &Path, wordsfile: &Path, word: String) -> std::io::Result<i32> {
+    println!("{:?}, {:?}", cmd, wordsfile);
 
     let mut i: i32 = 0;
-    let mut guess = String::new();
     let mut reply: String;
     let mut matched: String;
 
-    loop {
+    let (tx1, rx1): (Sender<String>, Receiver<String>) = channel();
+    let (tx2, rx2): (Sender<String>, Receiver<String>) = channel();
+
+    start_process(tx1, rx2, cmd, vec![wordsfile.to_str().unwrap()]);
+
+    for mut guess in rx1 {
+        println!("{}", guess);
         reply = String::new();
         matched = String::new();
-        match child_stdout {
-            Some(ref mut out) => { out.read_to_string(&mut guess)? },
-            None => { break }
-        };
+
+        guess = guess.chars().map(|c| c.to_ascii_lowercase()).collect();
 
         i += 1;
 
         for (ch_g, ch_w) in guess.chars().zip(word.chars()) {
+            println!("{}:{}", ch_g, ch_w);
             if ch_g == ch_w {
                 reply.push('g');
                 matched.push(ch_g);
-            } else if word.matches(ch_g).count() > 1 && matched.matches(ch_g).count() == 0 {
+            } else if word.find(ch_g).is_some() && matched.find(ch_g).is_none() {
                 reply.push('y');
             } else {
                 reply.push('b');
             }
+            println!("{}, {}", reply, matched);
         }
 
         reply.push('\n');
 
-        child_stdin.write_all(reply.as_bytes())?;
+        tx2.send(reply).unwrap();
     }
-
-    child.wait()?;
 
     Ok(i)
 }
@@ -65,9 +109,15 @@ fn test(cmd: String, word: String) -> std::io::Result<i32> {
 fn main() {
     let args = Args::parse();
 
-    let runs_s = fs::read_to_string(args.runfile).expect("Failed to read file");
-    let words_s = fs::read_to_string(args.wordsfile).expect("Failed to read file");
+    let runs_s = fs::read_to_string(&args.runfile).expect("Failed to read file");
+    let words_s = fs::read_to_string(&args.wordsfile).expect("Failed to read file");
 
-    let runs: Vec<String> = runs_s.split(" ").map(|s| s.to_string()).collect();
+    let runspath = &args.runfile.parent().unwrap();
+    let runs: Vec<PathBuf> = runs_s.split("\n").map(|s| PathBuf::from(s)).collect();
     let words: Vec<String> = words_s.split(" ").map(|s| s.to_string()).collect();
+
+    for c in runs.iter() {
+        let i = test(&runspath.join(c), &args.wordsfile, "franc".to_string()).unwrap();
+        println!("{:?}: {}", c, i);
+    }
 }
